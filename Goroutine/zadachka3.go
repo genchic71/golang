1e79afca13ef02305b7a0d6eec94d6a2bac341c3
genchic71 <tarungena30042006@gmail.com>
2026-02-04 15:09:19 +0300

    zadachka3.go

--- Goroutine/zadachka3.go
+++ Goroutine/zadachka3.go
@@ -6,12 +6,6 @@ import (
 )
 
 func main() {
-	//go func() {
-	//	for i := range 3 {
-	//		fmt.Println("Goroutine", i+1)
-	//	}
-	//}()
-	//time.Sleep(100 * time.Millisecond)
 	for i := 1; i < 4; i++ {
 		go func(i int) {
 			fmt.Println("Goroutine", i)
