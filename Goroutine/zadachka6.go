9065fc313e51f088f3fa3db3f7acd1ac7517ab34
genchic71 <tarungena30042006@gmail.com>
2026-02-04 15:10:19 +0300

    zadachka6.go

--- Goroutine/zadachka6.go
+++ Goroutine/zadachka6.go
@@ -5,15 +5,7 @@ import "fmt"
 func main() {
 	ch := make(chan string, 1)
 	done := make(chan bool, 1)
-	//go func() {
-	//	ch <- "Worker done"
-	//}()
-	//v, ok := <-ch
-	//if !ok {
-	//	return
-	//}
-	//fmt.Println(v)
-	//fmt.Println("Main continues")
+
 	go func(done chan bool) {
 		ch <- "Worker done"
 		done <- true
